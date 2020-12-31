90f2a9f21436ead9bcd04199f858927f9b44671e
apol0x <madejapolo@gmail.com>
2020-12-31 21:08:18 +0100

    ADD: init store & reducer

---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import  reducers from '../reducers';
+const initialState = {};
+//composición potenciada  para unir el middleware con el redux_devtools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+//Inicializamos la conexión con el redux-devtool
+//y exportamos la definición del store
+export const store = createStore(reducers, initialState, composeEnhancers(applyMiddleware(thunk)));
