b99aff937ead3726b4933dba14e79d24caa1f682
apol0x <madejapolo@gmail.com>
2020-12-31 21:07:51 +0100

    INIT: initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+
+class App extends Component {
+  render() {
+    return (
+     <div className="body">
+        Hola Mundo
+     </div>
+    );
+  }
+};
+
+export default App;
