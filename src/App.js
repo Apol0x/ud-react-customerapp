53ec45d71862072b36ff6b00fdfadf0938afafb8
apol0x <madejapolo@gmail.com>
2021-01-19 00:17:54 +0100

    MOD: route and switch from react-router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React, { Component } from 'react';
-import './App.css';
-
-
+import React, { Component } from "react";
+import { Link, BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import HomeContainer from './Containers/HomeContainer';
 
 class App extends Component {
+  renderHome = () => <h1>Home</h1>;
+  renderCustomerContainer = () => <h1>Customer Container</h1>;
+  renderCustomerListContainer = () => <h1>Customer List Container</h1>;
+  renderCustomerNewContainer = () => <h1>Customer New Container</h1>;
+
   render() {
     return (
-     <div className="body">
-        Hola Mundo
-     </div>
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={HomeContainer} />
+          <Route
+            exact
+            path="/customers"
+            component={this.renderCustomerContainer}
+          />
+          <Switch>
+            <Route
+              path="/customers/new"
+              component={this.renderCustomerNewContainer}
+            />
+            <Route
+              path="/customers/:dni"
+              component={this.renderCustomerListContainer}
+            />
+          </Switch>
+        </div>
+      </Router>
     );
   }
-};
+}
 
 export default App;
